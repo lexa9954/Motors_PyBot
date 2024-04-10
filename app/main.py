a4975dad079ac2440402d670be46ab98ab5b8f6c
senzzorr <senzzorr@gmail.com>
2024-04-10 15:00:55 +0600

    amt_proxy

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ import time
 
 bot = cfg.telebot.TeleBot(cfg.BOT_TOKEN, parse_mode='HTML')
 user_online = False
-
+print('БОТ ЗАПУЩЕН')
 
 
 @bot.message_handler(commands=['start'])
@@ -228,11 +228,10 @@ def notify_admin(txt=None):  # Оповещения админов
         time.sleep(15)
 
 if __name__ == '__main__':
-    #bot.infinity_polling(timeout=600,long_polling_timeout=5)
-    t1 = threading.Thread(target=bot.polling)
-    t2 = threading.Thread(target=notify_admin)
-    t1.start()
-    t2.start()
-    t1.join()
-    t2.join()
-    print('БОТ ЗАПУЩЕН')
+    bot.infinity_polling(timeout=600,long_polling_timeout=5)
+    #t1 = threading.Thread(target=bot.polling)
+    #t2 = threading.Thread(target=notify_admin)
+    #t1.start()
+    #t2.start()
+    #t1.join()
+    #t2.join()
